3b70cada1cad4ca9df30a0e5616e72a7b303a7b6
mgrsskls <mail@mgrossklaus.de>
2019-02-09 09:46:36 +0100

    added minifying etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
-const gulp = require("gulp");
-const eslint = require("gulp-eslint");
-const stylelint = require("gulp-stylelint");
 const babel = require("gulp-babel");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const rollup = require("rollup");
+const terser = require("rollup-plugin-terser");
 const postcss = require("gulp-postcss");
+const stylelint = require("gulp-stylelint");
 
-const jsFiles = ["js/*.js"];
-const cssFiles = ["css/index.css"];
-const jsDist = "build/js/";
-const cssDist = "build/css/";
+const buildFolder = "build/";
+const jsFiles = "js/index.js";
+const cssFiles = "css/index.css";
+const jsDist = `${buildFolder}js/`;
+const cssDist = `${buildFolder}css/`;
 
 gulp.task("lint:js", () =>
   gulp
@@ -33,16 +37,21 @@ gulp.task("watch:lint:css", () => {
   gulp.watch(cssFiles, gulp.parallel("lint:css"));
 });
 
-gulp.task("build:js", () =>
-  gulp
-    .src(jsFiles, { sourceMaps: true })
-    .pipe(
-      babel({
-        presets: ["@babel/env"]
-      })
-    )
-    .pipe(gulp.dest(jsDist, { sourcemaps: "." }))
-);
+gulp.task("build:js", () => {
+  return rollup
+    .rollup({
+      input: jsFiles,
+      plugins: [babel(), terser.terser()]
+    })
+    .then(bundle => {
+      return bundle.write({
+        file: `${jsDist}/index.js`,
+        format: "umd",
+        name: "library",
+        sourcemap: true
+      });
+    });
+});
 
 gulp.task("build:css", () =>
   gulp
@@ -51,8 +60,12 @@ gulp.task("build:css", () =>
     .pipe(gulp.dest(cssDist, { sourcemaps: "." }))
 );
 
-gulp.task("build", gulp.parallel("build:js", "build:css"));
+gulp.task("clean", () => {
+  return del([`${buildFolder}**/*`]);
+});
+
 gulp.task(
-  "default",
-  gulp.parallel("watch:lint:js", "watch:lint:css", "build:js", "build:css")
+  "build",
+  gulp.series("clean", gulp.parallel("build:js", "build:css"))
 );
+gulp.task("default", gulp.parallel("watch:lint:js", "watch:lint:css"));
